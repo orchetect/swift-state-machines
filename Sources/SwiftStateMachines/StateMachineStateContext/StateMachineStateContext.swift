448ee99d7ed72afcb7dbab7bfca744f8bb799e66
Steffan Andrews <stef@orchetect.com>
2026-07-07 10:45:30 -0700

    Removed initial buildout

--- Sources/SwiftStateMachines/StateMachineStateContext/StateMachineStateContext.swift
+++
@@ -1,9 +0,0 @@
-//
-//  StateMachineStateContext.swift
-//  SwiftStateMachines • https://github.com/orchetect/swift-state-machines
-//  © 2026 Steffan Andrews • Licensed under MIT License
-//
-
-public protocol StateMachineStateContext: Sendable {
-    static var defaultContext: Self { get }
-}
