1d0dd285d6d64c686c1ff308cb93e5f60749d902
Steffan Andrews <stef@orchetect.com>
2026-08-04 15:21:02 -0700

    Added `Collector` object

---
+++ Sources/SwiftStateMachines/Collector/Collector.swift
@@ -0,0 +1,92 @@
+//
+//  Collector.swift
+//  SwiftStateMachines • https://github.com/orchetect/swift-state-machines
+//  © 2026 Steffan Andrews • Licensed under MIT License
+//
+
+/// An object which publishes values to a receiver sequentially.
+/// Published values are collected as a backlog until a receiver is assigned.
+///
+/// Once a receiver is assigned, the backlog of collected values are passed to the receiver.
+/// After that, all values are directly passed to the receiver without being collected.
+public actor Collector<T: Sendable> {
+    public typealias Receiver = @Sendable (_ value: T) -> Void
+    var receiver: Receiver? = nil
+
+    var valuesBacklog: [T] = []
+
+    public init(
+        receiver: Receiver? = nil
+    ) {
+        self.receiver = receiver
+    }
+
+    deinit {
+        receiver = nil
+    }
+}
+
+// MARK: - Public
+
+extension Collector {
+    /// Publish a value to the receiver.
+    ///
+    /// If the receiver is not assigned, the value will be retained and then released
+    /// once a receiver is assigned.
+    nonisolated
+    public func publish(_ value: sending T) {
+        Task { @CollectorActor [value] in
+            await _send(value)
+        }
+    }
+
+    /// Set the receiver handler that values will be published to.
+    nonisolated
+    public func setReceiver(_ receiver: Receiver?) {
+        Task { @CollectorActor in
+            await _setReceiver(receiver)
+
+            guard let receiver else { return }
+            await _pushBacklog(to: receiver)
+        }
+    }
+
+    /// Returns `true` if the receiver handler has been set.
+    public var isReceiverSet: Bool {
+        receiver != nil
+    }
+}
+
+// MARK: - Internal
+
+extension Collector {
+    func _setReceiver(_ receiver: Receiver?) {
+        self.receiver = receiver
+    }
+
+    func _send(_ value: sending T) {
+        guard let receiver else {
+            _addToBacklog(value: value)
+            return
+        }
+
+        _pushBacklog(to: receiver)
+        _push(value: value, to: receiver)
+    }
+
+    func _addToBacklog(value: sending T) {
+        valuesBacklog.append(value)
+    }
+
+    func _push(value: sending T, to receiver: Receiver) {
+        receiver(value)
+    }
+
+    func _pushBacklog(to receiver: Receiver) {
+        let values = valuesBacklog
+        valuesBacklog.removeAll()
+        for value in values {
+            _push(value: value, to: receiver)
+        }
+    }
+}
